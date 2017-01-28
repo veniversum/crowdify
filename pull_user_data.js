872e51d08a972ece9c08e00f4a134c39d018ac13
Nick <nikita.karandej@gmail.com>
2017-01-28 18:54:51 +0000

    file cleanup

--- pull_user_data.js
+++ pull_user_data.js
@@ -4,25 +4,15 @@ var mongoose = require('mongoose');
 var spotifyApi = new SpotifyWebApi();
 
 var accessToken = 'BQCFiZQSo-3aFAaNy9Y6nDckwkYpb6RkAla_hxjxJfAeOYyxWXVjjwFPfU2cXAzr4BdZ8R_qqEqEPmgPodhetE-hXNUI-KGnYmtSguyLbFdJuiHBlLAYmq51TnSJLhicg1KbuaSlJLSVNMufU_Qw';
-spotifyApi.setAccessToken(accessToken);
 
-// var userId = "";
-// spotifyApi.getMe()
-//   .then(function(data) {
-//     console.log('Some information about the authenticated user', data.body);
-//     userID = "something";
-//   }, function(err) {
-//     console.log('Something went wrong!', err);
-// });
+spotifyApi.setAccessToken(accessToken);
 
 spotifyApi.getMyTopTracks(limit=2)
   .then(function(data) {
     var songList = data.body.items.map(function(item) {return item.name});
-    // console.log(songList);
     store_data(songList);
   }, function(err) {
     throw err;
-    console.log('theres an error', err);
   });
 
 function store_data(songList) {
@@ -40,7 +30,7 @@ function store_data(songList) {
   song.save(function(err) {
     if (err) throw err;
     console.log(song.songNames);
-    console.log('Added a new song list');
+    // console.log('Added a new song list');
   });
 
   mongoose.connection.close();
