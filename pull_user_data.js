8419492904c289565695d18179bb049b5acd636e
veniversum <hello@veniversum.me>
2017-01-28 20:13:28 +0000

    fix indentation in pull_user_data.js

--- pull_user_data.js
+++ pull_user_data.js
@@ -3,24 +3,24 @@ var SpotifyWebApi = require('spotify-web-api-node'),
    serverConfig = require('./serverConfig.json');
    
 function store_data(songList) {
- mongoose.Promise = global.Promise;
- mongoose.connect(serverConfig.mongo.host + serverConfig.mongo.schema);
- var Schema = mongoose.Schema;
- var songsSchema = new Schema({
-     songNames: [String]
- });
+   mongoose.Promise = global.Promise;
+   mongoose.connect(serverConfig.mongo.host + serverConfig.mongo.schema);
+   var Schema = mongoose.Schema;
+   var songsSchema = new Schema({
+       songNames: [String]
+   });
 
- var Song = mongoose.model('Song', songsSchema);
+   var Song = mongoose.model('Song', songsSchema);
 
- var song = new Song({songNames: songList});
+   var song = new Song({songNames: songList});
 
- song.save(function(err) {
-   if (err) throw err;
-   console.log(song.songNames);
-   // console.log('Added a new song list');
- });
+   song.save(function(err) {
+     if (err) throw err;
+     console.log(song.songNames);
+     // console.log('Added a new song list');
+   });
 
- mongoose.connection.close();
+   mongoose.connection.close();
 }
    
 exports.pullAttendeeData = function(accessToken) {
