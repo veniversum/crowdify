cd789774bcd9319ea18b3e62262573fed9cd3a13
Chris Cundy <chris.j.cundy@gmail.com>
2017-01-28 19:57:43 +0000

    split functions out into separate files

--- music_skeleton.js
+++ music_skeleton.js
@@ -9,36 +9,24 @@ var spotifyApi = new SpotifyWebApi({
 
 spotifyApi.setAccessToken('BQAqbsMYn0w_CxdULL3xd1mm7urAgPCM0L3PZPNrC-BV2CTiDLVkk4akPYtBuJUERagNKi90xCC0ZNfttLmE-NP2DivRBe6U7UAojDLXnbqDEVJRDHq0MqZW2MGKjqpz6zPaMuQcQV79RMBxmyqD2p95QF9amL0PJTDJm1fi31DckLOup7YDzvpziSbDsBK67bCmQ6jAla0mrYqfiQsDlFIDwqrPteG3dPbKRSBmAoBINIr2NFcczxU-cPuQfEkF231IPYVCk_2YwflPY2_yzAoVDSYUmwv-W4FaiQnd-W-CTkv5lnQ')
 
-function getFromDB(){
-}
 
-
-
-function getRecommendations(seed_artists){
+function getRecommendations(seed_tracks){
     var argument = {min_energy: 0.4,
-		    seed_artists: seed_artists,
+		    seed_artists: seed_tracks,
 		    min_popularity: 50}
     var recommendations = spotifyApi.getRecommendations(argument)
     return recommendations
 }
 
-function createPlaylist(userId, playlistName){
-    spotifyApi.createPlaylist(userId, playlistName, { 'public' : false })
-	.then(function(data) {
-	    console.log('Created playlist!');
-	}, function(err) {
-	    console.log('Something went wrong in creating a playlist!', err);
-	});
-}
+
 
 function updatePlaylist(songArray, userId, playlistId){   
     spotifyApi.addTracksToPlaylist(userId, playlistId, songArray).
 	then(function(data) {console.log("Playlist updated!");},
-	     function(err) {console.log("something went wrong in the playlist update");})
-	     
+	     function(err) {console.log("something went wrong in the playlist update");})	     
 }
 
-function recommendAndUpdate(userId, playlistId, seed_artists){
+function recommendAndUpdate(userId, playlistId, seed_tracks){
     // Generate recommendations based on seed artists, then
     // add these songs to the playlist
     getRecommendations(seed_artists).
@@ -53,6 +41,5 @@ function recommendAndUpdate(userId, playlistId, seed_artists){
 
 recommendAndUpdate('aqarias', 'myCoolPlaylist5', ['6mfK6Q2tzLMEchAr0e9Uzu',
 				     '4DYFVNKZ1uixa6SQTvzQwJ'])
-
 
 
